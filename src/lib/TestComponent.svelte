1ba3baebefa4829a5a9d706b1843d88aae46965d
benedikt <benedikt.mayer@st.ovgu.de>
2024-11-20 16:40:55 +0100

    trying to make things more performant

--- src/lib/TestComponent.svelte
+++ src/lib/TestComponent.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { geoPath, geoMercator } from 'd3-geo';
+
 	let { svgLayer = $bindable(), map, mapMoveNotifyToggle, data } = $props();
 
 	let lng = -7.807195714694519;
@@ -8,13 +10,28 @@
 	let cy = $state(0);
 
 	let pathData = $state([]);
-	$inspect(pathData);
+	// $inspect(pathData);
 
 	console.log(data);
 
 	$effect(() => {
+		console.log('updating view');
 		const pathDataTmp = [];
 
+		const mümap = map;
+		debugger;
+
+		// TODO: This is just much more performat: https://observablehq.com/@d3/zoomable-raster-vector?collection=@d3/d3-zoom
+		// const projection = geoMercator()
+		// 	.scale(1 / (2 * Math.PI))
+		// 	.translate([0, 0]);
+
+		// const render = geoPath(projection);
+
+		// // const render = geoPath(map.projection);
+		// const müData = data;
+		// const mue = render(data.features);
+		// debugger;
 		data.features.forEach((feature) => {
 			const coordinates = feature.geometry.coordinates[0];
 			const pathString =
@@ -40,9 +57,11 @@
 <svg bind:this={svgLayer} class="test-svg" width="100%" height="100%">
 	<rect width="100" height="100" fill="red"> </rect>
 	<circle {cx} {cy} r="10" fill="blue"></circle>
-	{#each pathData as pathString}
-		<path d={pathString} fill="none" stroke="red"></path>
-	{/each}
+	<g class="region-paths-g">
+		{#each pathData as pathString}
+			<path d={pathString} fill="none" stroke="red"></path>
+		{/each}
+	</g>
 </svg>
 
 <style>

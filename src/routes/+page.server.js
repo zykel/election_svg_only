3231f731fea497e6a750a2c7c9b072b240cb4809
benedikt <benedikt.mayer@st.ovgu.de>
2024-11-22 10:46:26 +0100

    different map resolution

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,7 @@
 export const load = async ({ fetch }) => {
 	// https://mapshaper.org/ to simplify geojson files
-	const response = await fetch('electoral_map_seats_voronoi_10perc_geo.json');
+	// const response = await fetch('electoral_map_seats_voronoi.json');
+	const response = await fetch('electoral_map_seats_voronoi_20perc_geo.json');
 	let geodataVoronoi = await response.json();
 
 	return { geodataVoronoi };

5d1fe3997a12231d19757794a5da313080fcbc67
benedikt <benedikt.mayer@st.ovgu.de>
2024-11-26 14:14:49 +0100

    working with object (but not feeling super clean)

--- src/lib/SvgLayer.svelte
+++ src/lib/SvgLayer.svelte
@@ -4,51 +4,17 @@
 	import { zoom, zoomIdentity } from 'd3-zoom';
 	import { onMount } from 'svelte';
 	import { rewind } from '@turf/rewind';
-	import { setupMap } from '$lib/helperMap.svelte.js';
+	import { getMapHelper, pathDataMap } from '$lib/helperMap.svelte.js';
 
 	let { svgLayer = $bindable(), data, mapWidth, mapHeight } = $props();
 
-	// Following this https://observablehq.com/@d3/zoomable-raster-vector?collection=@d3/d3-zoom
-
-	// For us : [-98.58333333333333, 39.833333333333336]
-
-	const zoomHelper = zoom()
-		.scaleExtent([1 << 2, 1 << 20])
-		.extent([
-			[0, 0],
-			[mapWidth, mapHeight]
-		])
-		.on('zoom', ({ transform }) => zoomed(transform));
-
-	function zoomed(transform) {
-		projection = geoMercator() // redudant?
-			.center([lngInitial, latInitial]) // redudant?
-			.scale(transform.k)
-			.translate([transform.x, transform.y]);
-	}
-
-	const mapHelper = $derived(
-		getMapHelper(svgLayer, data, mapWidth, mapHeight, lngInitial, latInitial, scaleInitial)
-	);
+	const mapHelper = $derived(getMapHelper(svgLayer, data, mapWidth, mapHeight));
 
 	onMount(() => {
-		mapHelper.setupMap();
+		mapHelper.setupZoom();
 	});
 
-	function getPathData(projection) {
-		const pathDataTmp = [];
-
-		const pathGenerator = geoPath(projection);
-
-		data.features.forEach((feature, i) => {
-			const pathString = pathGenerator(feature);
-			pathDataTmp.push({ id: feature.id, area_seat: feature.properties.area_seat, pathString });
-		});
-
-		return pathDataTmp;
-	}
-
-	let pathData = $derived(getPathData(projection));
+	const pathData = $derived(pathDataMap.value);
 </script>
 
 <svg bind:this={svgLayer} class="test-svg" width="100%" height="100%">
--- src/lib/helperMap.svelte.js
+++ src/lib/helperMap.svelte.js
@@ -1,23 +1,132 @@
-const lngInitial = -7.807195714694519;
-const latInitial = 53.41035563891312;
-const scaleInitial = 1 << 12;
+import { geoPath, geoMercator, geoTransform } from 'd3-geo';
+import { select, selectAll } from 'd3';
+import { zoom, zoomIdentity } from 'd3-zoom';
 
-let projection = $state(
-	geoMercator()
-		.scale(scaleInitial)
-		.translate([mapWidth / 2, mapHeight / 2])
-		.center([lngInitial, latInitial])
-);
+const lng = -7.807195714694519;
+const lat = 53.41035563891312;
+
+// export class MapHelper {
+// 	svgLayer = $state(null);
+// 	data = $state(null);
+// 	mapWidth = $state(0);
+// 	mapHeight = $state(0);
+
+// 	translateX = $state(0);
+// 	translateY = $state(0);
+
+// 	scale = $state(1 << 12);
+// 	projection = geoMercator().center([lng, lat]);
+
+// 	zoomHelper = zoom()
+// 		.scaleExtent([1 << 2, 1 << 20])
+// 		.extent([
+// 			[0, 0],
+// 			[mapWidth, mapHeight]
+// 		])
+// 		.on('zoom', ({ transform }) => this.zoomed(transform));
+
+// 	constructor(svgLayer, data, mapWidth, mapHeight) {
+// 		this.svgLayer = svgLayer;
+// 		this.data = data;
+// 		this.mapWidth = mapWidth;
+// 		this.mapHeight = mapHeight;
+
+// 		this.translateX = mapWidth / 2;
+// 		this.translateY = mapHeight / 2;
+// 	}
+
+// 	setupZoom() {
+// 		this.zoomHelper(select(this.svgLayer));
+// 		this.zoomHelper.transform(
+// 			select(this.svgLayer),
+// 			zoomIdentity.translate(this.mapWidth / 2, this.mapHeight / 2).scale(this.scale)
+// 		);
+// 	}
+
+// 	zoomed(transform) {
+// 		this.scale = transform.k;
+// 		this.translateX = transform.x;
+// 		this.translateY = transform.y;
+// 	}
+
+// 	getPathData() {
+// 		const pathDataTmp = [];
+
+// 		const pathGenerator = geoPath(this.projection);
+
+// 		this.data.features.forEach((feature, i) => {
+// 			const pathString = pathGenerator(feature);
+// 			pathDataTmp.push({
+// 				id: feature.id,
+// 				area_seat: feature.properties.area_seat,
+// 				pathString
+// 			});
+// 		});
+
+// 		return pathDataTmp;
+// 	}
+
+// 	get pathData() {
+// 		this.projection.scale(this.scale).translate([this.translateX, this.translateY]);
+
+// 		return this.getPathData();
+// 	}
+// }
+
+export const pathDataMap = $state({ value: [] });
+const projection = geoMercator().center([lng, lat]);
+
+let translateX = $state(0);
+let translateY = $state(0);
+let scale = $state(1 << 12);
 
 export const getMapHelper = (svgLayer, data, mapWidth, mapHeight) => {
+	const lng = -7.807195714694519;
+	const lat = 53.41035563891312;
+
+	const zoomHelper = zoom()
+		.scaleExtent([1 << 2, 1 << 20])
+		.extent([
+			[0, 0],
+			[mapWidth, mapHeight]
+		])
+		.on('zoom', ({ transform }) => zoomed(transform));
+
+	function zoomed(transform) {
+		scale = transform.k;
+		translateX = transform.x;
+		translateY = transform.y;
+
+		projection.scale(scale).translate([translateX, translateY]);
+		pathDataMap.value = mapHelper.getPathData();
+	}
+
+	// Construct functions and variables to access from outside
 	const mapHelper = {};
 
-	mapHelper.setupMap = () => {
+	mapHelper.setupZoom = () => {
 		zoomHelper(select(svgLayer));
 		zoomHelper.transform(
 			select(svgLayer),
 			zoomIdentity.translate(mapWidth / 2, mapHeight / 2).scale(scale)
 		);
+	};
+
+	mapHelper.getPathData = () => {
+		const pathDataTmp = [];
+
+		const pathGenerator = geoPath(projection);
+
+		data.features.forEach((feature, i) => {
+			const pathString = pathGenerator(feature);
+			pathDataTmp.push({
+				id: feature.id,
+				area_seat: feature.properties.area_seat,
+				pathString
+			});
+		});
+
+		return pathDataTmp;
 	};
 
 	return mapHelper;

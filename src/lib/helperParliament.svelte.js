b355ab267ff2b99c463674042d1afb44df24b55a
benedikt <benedikt.mayer@st.ovgu.de>
2024-11-27 12:36:41 +0100

    handcrafted parliament working out

--- src/lib/helperParliament.svelte.js
+++ src/lib/helperParliament.svelte.js
@@ -1,13 +1,46 @@
 import { range } from 'd3-array';
 
 export const getParliamentHelper = (mapWidth, mapHeight) => {
-	const pathData = range(175).map((d, idx) => {
-		const amp = 200;
-		const cx = mapWidth / 2 + amp * Math.cos((d / 175) * 2 * Math.PI);
-		const cy = mapHeight / 2 + amp * Math.sin((d / 175) * 2 * Math.PI);
+	const nrSeatsPerRow = {
+		0: 22,
+		1: 27,
+		2: 29,
+		3: 30,
+		4: 32,
+		5: 34
+	};
+	const rows = Object.keys(nrSeatsPerRow);
+
+	const getPositionsArray = (row) => {
+		const r = 150 + 25 * row;
+		const nrSeats = nrSeatsPerRow[row];
+		const positions = range(nrSeats).map((seatNr) => {
+			const angle = (seatNr / (nrSeats - 1)) * Math.PI - Math.PI;
+			const x = mapWidth / 2 + r * Math.cos(angle);
+			const y = mapHeight * 0.75 + r * Math.sin(angle);
+			return { seatNr, x, y, angle, row };
+		});
+		return positions;
+	};
+
+	let positions = [
+		...getPositionsArray(0),
+		...getPositionsArray(1),
+		...getPositionsArray(2),
+		...getPositionsArray(3),
+		...getPositionsArray(4),
+		...getPositionsArray(5)
+	];
+
+	positions.sort((a, b) => (a.angle < b.angle ? -1 : 1));
+
+	const pathData = range(174).map((d, idx) => {
+		const position = positions[idx];
+		const x = position.x;
+		const y = position.y;
 		const r = 10;
 		// create an svg path to depict a circle at cx, cy with radius r
-		const pathString = `M ${cx} ${cy} A ${r} ${r} 0 1 1 ${cx + 0.1} ${cy}`;
+		const pathString = `M ${x} ${y} A ${r} ${r} 0 1 1 ${x + 0.1} ${y}`;
 
 		// pathString = `M ${x} ${y} A ${r} ${r} 0 1 1 ${x + 0.1} ${y}`;
 		return { idx, area_seat: 'dummy-circle', pathString };

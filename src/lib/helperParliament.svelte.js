38edfe57f04c37eff8bef99d9801dd6ca7993745
benedikt <benedikt.mayer@st.ovgu.de>
2024-12-06 12:00:54 +0100

    horseshoe parliament working

--- src/lib/helperParliament.svelte.js
+++ src/lib/helperParliament.svelte.js
@@ -3,24 +3,44 @@ import { parties } from '$lib/p.svelte.js';
 
 const getPositions = (mapWidth, mapHeight) => {
 	const nrSeatsPerRow = {
-		0: 22,
-		1: 27,
-		2: 29,
-		3: 30,
-		4: 32,
-		5: 34
+		0: 29,
+		1: 33,
+		2: 36,
+		3: 37,
+		4: 39
 	};
+	const nrSeatsStraightSide = 8;
+
 	const rows = Object.keys(nrSeatsPerRow);
+	const nrSeatsPerRowCurved = rows.map((row) => nrSeatsPerRow[row] - 2 * nrSeatsStraightSide);
+
+	const rowDistance = 25;
 
 	const getPositionsArray = (row) => {
-		const r = 150 + 25 * row;
-		const nrSeats = nrSeatsPerRow[row];
-		const positions = range(nrSeats).map((seatNr) => {
-			const angle = (seatNr / (nrSeats - 1)) * Math.PI - Math.PI;
+		const r = 100 + rowDistance * row;
+		const nrSeatsCurved = nrSeatsPerRowCurved[row];
+		const positionsCurved = range(nrSeatsCurved).map((seatNr) => {
+			const angle = (seatNr / (nrSeatsCurved - 1)) * Math.PI - Math.PI;
 			const x = mapWidth / 2 + r * Math.cos(angle);
-			const y = mapHeight * 0.75 + r * Math.sin(angle);
+			const y = mapHeight * 0.5 + r * Math.sin(angle);
 			return { seatNr, x, y, angle, row };
 		});
+		const yOffset = rowDistance;
+		const positionsStraightLeft = range(nrSeatsStraightSide).map((seatNr) => {
+			const x = positionsCurved[0].x;
+			const y = positionsCurved[0].y + yOffset * seatNr;
+			// Just for ordering purposes
+			const angle = -Math.PI - (1 + seatNr + 0.1 * row);
+			return { seatNr, x, y, angle, row };
+		});
+		const positionsStraightRight = range(nrSeatsStraightSide).map((seatNr) => {
+			const x = positionsCurved[positionsCurved.length - 1].x;
+			const y = positionsCurved[0].y + yOffset * seatNr;
+			// Just for ordering purposes
+			const angle = 1 + seatNr + 0.1 * row;
+			return { seatNr, x, y, angle, row };
+		});
+		const positions = [...positionsStraightLeft, ...positionsCurved, ...positionsStraightRight];
 		return positions;
 	};
 
@@ -29,6 +49,7 @@ const getPositions = (mapWidth, mapHeight) => {
 	// To ensure that the seats for different parties are in block-like order, sort them by angle
 	positions.sort((a, b) => (a.angle < b.angle ? -1 : 1));
 
+	console.log('positions length', positions.length);
 	return positions;
 };
 
@@ -75,7 +96,6 @@ export const getParliamentHelper = (data, mapWidth, mapHeight) => {
 	const pathData = data.features.map((feature, idx) => {
 		const groupedIdx = getGroupedIdx(feature.properties.party);
 		const position = positions[groupedIdx];
-		console.log('groupedIdx', groupedIdx);
 		const x = position.x;
 		const y = position.y;
 		const r = 10;

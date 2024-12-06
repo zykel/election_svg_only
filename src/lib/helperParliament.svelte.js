f4fa377175dd64534e2b4fad04d479d63e93118c
benedikt <benedikt.mayer@st.ovgu.de>
2024-12-06 11:11:07 +0100

    properly ordering the parliament chart

--- src/lib/helperParliament.svelte.js
+++ src/lib/helperParliament.svelte.js
@@ -1,4 +1,5 @@
 import { range } from 'd3-array';
+import { parties } from '$lib/p.svelte.js';
 
 const getPositions = (mapWidth, mapHeight) => {
 	const nrSeatsPerRow = {
@@ -31,11 +32,50 @@ const getPositions = (mapWidth, mapHeight) => {
 	return positions;
 };
 
+const getPartySubsetIdxCounter = (data) => {
+	const nrSeatsPerParty = {};
+	parties.forEach((party) => {
+		nrSeatsPerParty[party] = 0;
+	});
+	data.features.forEach((feature) => {
+		const party = feature.properties.party;
+		nrSeatsPerParty[party]++;
+	});
+
+	const subsetOffsets = {};
+	parties.forEach((party, i) => {
+		//  Get sum of all preceding parties' seat counts
+		let offset = 0;
+		parties.forEach((otherParty, j) => {
+			if (i > j) offset += nrSeatsPerParty[otherParty];
+		});
+		subsetOffsets[party] = offset;
+	});
+
+	const counter = {};
+	const getGroupedIdx = (party) => {
+		let idxInSubset = 0;
+		if (!counter[party]) {
+			counter[party] = 0;
+		} else {
+			idxInSubset = counter[party];
+		}
+		counter[party]++;
+
+		return idxInSubset + subsetOffsets[party];
+	};
+
+	return getGroupedIdx;
+};
+
 export const getParliamentHelper = (data, mapWidth, mapHeight) => {
 	const positions = getPositions(mapWidth, mapHeight);
+	const getGroupedIdx = getPartySubsetIdxCounter(data);
 
 	const pathData = data.features.map((feature, idx) => {
-		const position = positions[idx];
+		const groupedIdx = getGroupedIdx(feature.properties.party);
+		const position = positions[groupedIdx];
+		console.log('groupedIdx', groupedIdx);
 		const x = position.x;
 		const y = position.y;
 		const r = 10;

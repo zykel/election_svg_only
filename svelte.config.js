c0c8d2b06edd6e2827401884571e5746e5fdfcf1
benedikt <benedikt.mayer@st.ovgu.de>
2024-12-13 10:39:56 +0100

    again trying

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,9 @@ export default {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
